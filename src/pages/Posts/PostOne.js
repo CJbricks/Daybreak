5ea20f18bfa1d87d824d016d4f2c8bdda1f6c185
C.J. Christian <118404402+CJbricks@users.noreply.github.com>
2023-03-24 12:24:26 -0600

    Added text and styling to Post components.

--- src/pages/Posts/PostOne.js
+++ src/pages/Posts/PostOne.js
@@ -15,7 +15,20 @@ export default function PostOne() {
                         height={675}
                         class={styles.image} />
                     </div>
-                    <div class={styles.second}></div>
+                    <div class={styles.second}>
+                        <p class={styles.label}>Lunch Table.</p>
+                        <br />
+                        <p class={styles.textbody}>I always found the wildest kids at the lunch table. A true mix of cultural background and 
+                            the true sociological enviroment for male development. Lord of the flies meets the free market and I believe
+                            that the market will solve everything. If left un tampered and free to develop as naturally as possible, the Derek Huntsman archetype 
+                            will reign. I used to believe this idea was a bad idea, but now I have come around to respect the nature in which the world works.
+                        <br />
+                        <br />
+                        Free from outside influence we would probably
+                        all just be burning ourselves with lighters.    
+
+                        </p>
+                    </div>
                 </div>
             </div>
     )
