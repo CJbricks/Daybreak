448d1ace6a14ef07ca34976727ece02ecded795c
C.J. Christian <118404402+CJbricks@users.noreply.github.com>
2023-03-29 22:16:14 -0600

    Added input mono font to etxt body.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c452f0ca359cf34a"; }
+/******/ 		__webpack_require__.h = function() { return "d0cc21fdba037ab3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
