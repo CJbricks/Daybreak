8b19c8e25e88b25a4525977451f544d0adcd5e4c
C.J. Christian <118404402+CJbricks@users.noreply.github.com>
2023-04-02 09:17:25 -0600

    Updated CSS modules

--- .next/static/webpack/webpack.8ca8b4c8bcd935fd.hot-update.js
+++ .next/static/webpack/webpack.8ca8b4c8bcd935fd.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c905ae9f5626af42"; }
+/******/ 	__webpack_require__.h = function() { return "a678a1c26917cc99"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
---
+++ src/pages/Posts/Ripoffs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '@/styles/Seperator.module.css';
+
+export default function () {
+  return (
+    <div className={styles.carousel}>
+        
+    </div>
+
+  )
+}
