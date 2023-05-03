cef34cabf3031238ff30c1fef3f95188b4627348
C.J. Christian <118404402+CJbricks@users.noreply.github.com>
2023-05-03 17:10:47 -0600

    Changing main component styling with chakra

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7ff8460363369f85"; }
+/******/ 		__webpack_require__.h = function() { return "501d6d031216617a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.685bb1d5949cc1b2.hot-update.js
+++ .next/static/webpack/webpack.685bb1d5949cc1b2.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "72579662f9dc60ac"; }
+/******/ 	__webpack_require__.h = function() { return "501d6d031216617a"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
