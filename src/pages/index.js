2d543d02afabe2910347cdf38c3499478308a13e
C.J. Christian <118404402+CJbricks@users.noreply.github.com>
2023-10-11 20:34:15 -0500

    Meta data

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
         <title>Daybreak | Independant Art Magazine</title>
         <meta name="description" content="PSD Drawings created by artist C.J. Christian | Independant Magazine publication" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:type" content="website" />
         <meta property="og:title" content= "Daybreak | Independant Art Publication"/>
         <meta property="og:description" content="Independant Art Magazine, Publication, and Printing." />
         <meta property="og:image" content="https://live.staticflickr.com/65535/53251589491_11d3408f4d_m.jpg" />
